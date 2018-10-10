3a5249213c17f0eb6ee30300f1722444b40dcc8c
barmaleysk <43924454+barmaleysk@users.noreply.github.com>
2018-10-10 17:54:02 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -187,7 +187,7 @@ bot.on('callback_query', msg => {
 
           bot.answerCallbackQuery({
             callback_query_id: msg.id,
-            text: `Показаны все ${itemText}`
+            text: ``
           }).then(() => QueryController.findByQuery(user, showItem))
           break
 
