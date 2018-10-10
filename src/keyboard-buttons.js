e2748d0aaa76369f01ac89987b344ba48dac6d70
narntoday <40935229+narntoday@users.noreply.github.com>
2018-10-10 13:52:31 +0400

    5

---
+++ src/keyboard-buttons.js
@@ -0,0 +1,19 @@
+module.exports = {
+  home: {
+    bouqets: '🔥 Реклама',
+    packaging: '☄️ Упаковка',
+    bro: 'Отправить другу 👍🏻',
+    bots: '💥 Контент',
+    cart: '🤝 Заказы'
+  },
+  cart: {
+    clear: '🗑️ Очистить корзину',
+    order: '🌸 Оформить заказ'
+  },
+  reasons: {
+    birthday: '👌 Старт',
+    jubilee: '✊ Старт',
+    wedding: '⚡ Старт',
+    love: '🔥 Старт'
+  }
+}
