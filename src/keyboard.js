b5f36eded6e45dfd23aa34cbe39a11f19f1ef826
barmaleysk <43924454+barmaleysk@users.noreply.github.com>
2018-10-10 15:43:12 +0400

    Update keyboard.js

--- src/keyboard.js
+++ src/keyboard.js
@@ -3,7 +3,7 @@ const kb = require ('./keyboard-buttons')
 module.exports = {
   home: [
     [kb.home.packaging],
-    [kb.home.bots, kb.home.packaging],
+    [kb.home.bots, kb.home.ads],
     [kb.home.cart],
 	   [kb.home.money],
 	  [kb.home.bro]
